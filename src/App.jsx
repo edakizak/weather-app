3a43eb4a12006998733e5ec8e8a7152382d3a2df
edakizak <edakizak.de@gmail.com>
2024-01-22 16:52:41 +0100

    Add Activity Form

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Form from "./components/form/Form.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <Form />
+    </>
+  );
 }
 
 export default App;
