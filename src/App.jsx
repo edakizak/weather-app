51f6b8ed45f0202253d7590af185022aa94950af
edakizak <edakizak.de@gmail.com>
2024-01-23 12:30:32 +0100

    Added fetch Url

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,41 @@ import Form from "./components/form/Form.jsx";
 import { uid } from "uid";
 import useLocalStorageState from "use-local-storage-state";
 import List from "./components/list/List.jsx";
+import { useEffect, useState } from "react";
 
 function App() {
   const [activities, setActivities] = useLocalStorageState("activities", {
     defaultValue: [],
   });
+  const [weather, setWeather] = useState(null);
+  const isGoodWeather = true; // In the App.js, add a variable const isGoodWeather = true.
+  const filteredActivities = activities.filter(
+    (activity) => activity.isGoodWeather === isGoodWeather
+  ); //Filter the activities for those whose key isForGoodWeather is equal to the global isGoodWeather variable.
+
+  const weatherUrl = `https://example-apis.vercel.app/api/weather`;
+  useEffect(() => {
+    async function fetchWeatherData() {
+      try {
+        const response = await fetch(weatherUrl);
+        if (response.ok) {
+          const weatherData = await response.json();
+
+          setWeatherData(weatherData);
+        } else {
+          console.error("");
+        }
+      } catch (error) {
+        console.error("Error!");
+      }
+    }
+
+    const intervalID = setInterval(fetchWeatherData, 5000);
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, [weatherUrl]);
+
   function handleAddActivity(newActivity) {
     setActivities([{ id: uid(), ...newActivity }, ...activities]);
     // create a state for activities,
