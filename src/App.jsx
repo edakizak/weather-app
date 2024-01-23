456f1dd9c7ed2eb41c47338d85edec94ebed9477
edakizak <edakizak.de@gmail.com>
2024-01-23 11:24:52 +0100

    fixed delete button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import React from "react";
 import "./App.css";
-import Form from "./components/form/Form.jsx";
+import Form from "./components/Form/Form.jsx";
 import { uid } from "uid";
 import useLocalStorageState from "use-local-storage-state";
 import List from "./components/list/List.jsx";
@@ -11,16 +9,25 @@ function App() {
   const [activities, setActivities] = useLocalStorageState("activities", {
     defaultValue: [],
   });
-  function handleAddActivity(newActivities) {
-    setActivities([{ id: uid(), ...newActivities }, ...activities]);
+  function handleAddActivity(newActivity) {
+    setActivities([{ id: uid(), ...newActivity }, ...activities]);
     // create a state for activities,
     // write a function handleAddActivity which accepts a new activity object as parameter and
     // adds this object to the activities state
     // please add a unique id to every new activity object; you can use uid to do so.
   }
+
+  function handleDeleteActivity(activityId) {
+    if (activityId !== null) {
+      setActivities(
+        activities.filter((activity) => activity.id !== activityId)
+      );
+    }
+  }
   return (
     <>
       <Form onAddActivity={handleAddActivity} />
+      <List activities={activities} deleteActivity={handleDeleteActivity} />
       {/* <List
         isGoodWeather={isGoodWeather}
         activities={
